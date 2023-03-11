52f41904fb218c2a9f0dbc05d5814eb232bb7e07
Dusan Odalovic <dodalovic@gmail.com>
2023-03-11 14:44:27 +0100

    Add credits page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,6 +120,10 @@ const config = {
                 label: 'GitHub',
                 href: 'https://github.com/gran-software-solutions/rest-api-guidelines',
               },
+              {
+                label: 'Credits',
+                href: '/credits'
+              }
             ],
           },
         ],
